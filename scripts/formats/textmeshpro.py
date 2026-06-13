0d6a7ab06f87c57e451c25e985f98d7d758d915c
Devon (Scarlet Cafe/Gorialis) R - Helping Hands <sansgorialis@gmail.com>
2026-06-13 15:42:07 +0100

    Make presenter show in fixed position

--- scripts/formats/textmeshpro.py
+++ scripts/formats/textmeshpro.py
@@ -102,7 +102,7 @@ DISPLAY_TIMEZONES_SPECIAL = [(ZoneInfo(iana), alpha) for (iana, alpha) in [
 ]]
 
 EVENT_TEXT_SPECIAL = """
-<size=120%>\uE00B</size> <b>{event_name}</b>                      <size=70%>担当者/Presenter: </size> {presenter}
+<size=120%>\uE00B</size> <b>{event_name}</b><pos=40%><size=70%>担当者/Presenter: </size> {presenter}</pos>
 {timezones}
 """.strip()
 
